f544c174ccd11061ce12193297c709e52831a19d
Habba101 <rahabamotaung@yahoo.com>
2026-07-23 18:06:57 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="container">
       <h1>Weather App</h1>
 
     <footer>
@@ -11,7 +12,7 @@ export default function App() {
        open-sourced on GitHub
       </a>
     </footer>
-
+</div> 
     </div>
   );
 }
