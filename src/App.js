bcb79b4742335c7d3ffb025e3e4cf591611bc75a
Habba101 <rahabamotaung@yahoo.com>
2026-07-23 16:21:18 +0200

    Added a footer with a Github link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-    Weather App
+      <h1>Weather App</h1>
+
+    <footer>
+      This project was coded by Rahaba Motaung  and is {""}
+      <a href="https://github.com/Habba101/react-weather-app" target="_blank" rel="noopener noreferrer">
+       open-sourced on GitHub
+      </a>
+    </footer>
+
     </div>
   );
 }
